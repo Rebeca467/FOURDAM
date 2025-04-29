259c05e067e6e68cc56313d567d9fbcc8038ff51
MaxwellRoyers <oriolfs2005@gmail.com>
2025-04-29 19:10:46 +0200

    prueba para ver si funciona todo bien

---
+++ PruebaHelloWorld/src/main/java/reto/pruebahelloworld/PruebaHelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package reto.pruebahelloworld;
+
+/**
+ *
+ * @author DAM106
+ */
+public class PruebaHelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
