fe4cc27ff2e3870b966f808e3a94ee2c61a4e1f4
CiroGalanVertiz <152623830+CiroGalanVertiz@users.noreply.github.com>
2025-04-29 19:25:39 +0200

    hola
    hola

---
+++ pruebaCiro/src/main/java/com/mycompany/pruebaciro/PruebaCiro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebaciro;
+
+/**
+ *
+ * @author DAM120
+ */
+public class PruebaCiro {
+
+    public static void main(String[] args) {
+        System.out.println("Hola wenas");
+    }
+}
