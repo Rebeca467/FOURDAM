effe0e90e5a1ab8cf306bdb6b527eeb3461fbbf6
Rebeca467 <rebecacaboo14@gmail.com>
2025-04-29 19:48:15 +0200

    Holaaaaaaa
    g

---
+++ prueba/src/main/java/reto/prueba/Prueba.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package reto.prueba;
+
+/**
+ *
+ * @author DAM124
+ */
+public class Prueba {
+
+    public static void main(String[] args) {
+        System.out.println("Rama de Fabian");
+        System.out.println("Prueba");
+        System.out.println("hola wenas");
+        System.out.println("Prueba 4");
+    }
+}
