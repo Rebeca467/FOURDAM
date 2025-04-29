53e61786120e2d008a4eb4541741ae56539c2f25
MaxwellRoyers <oriolfs2005@gmail.com>
2025-04-29 19:14:29 +0200

    Segunda prueba

--- PruebaHelloWorld/src/main/java/reto/pruebahelloworld/PruebaHelloWorld.java
+++ PruebaHelloWorld/src/main/java/reto/pruebahelloworld/PruebaHelloWorld.java
@@ -11,6 +11,6 @@ package reto.pruebahelloworld;
 public class PruebaHelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hellouda World!");
     }
 }
